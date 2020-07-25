22b3ba23747796bbb8f86d1f492830c009334f5b
CodyHoover <codydevloop@gmail.com>
2020-07-25 06:58:23 -0700

    building AboutMe CardComponent mdbreact

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,15 +1,27 @@
-import React from "react";
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
-
-
+import React from 'react';
+import { MDBBtn, MDBCard, MDBRow, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBContainer } from 'mdbreact';
+import "../AboutMe/AboutMe.css";
+import Cody from "../../assets/CodyPortfolio.png";
 const AboutMe = () => {
     return (
-        <>
+        <MDBContainer fluid className="test mt-5 pt-5">
+            <MDBRow>
+                <MDBCol size="10">
+                    <MDBRow>
+                    <MDBCol size="4">
+                    <img src={Cody} className={"img-fluid"} alt="" />
 
+                    </MDBCol>
+                    <MDBCol size="8">
+                    <p>test</p>
+                    </MDBCol>
+                    </MDBRow>
 
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
 
-        </>
     )
-};
+}
 
 export default AboutMe;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,29 +1,58 @@
-import React from "react"
-import "./Nav.css";
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import React from 'react';
+import { MDBContainer, MDBRow, MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon, MDBCol } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+import "../Nav/Nav.css"
+import AboutMe from "../AboutMe/AboutMe";
 
-const Nav= () => {
-    return (
-    <>
-                <MDBContainer fluid className="container">
-                <MDBRow>
-                    <MDBCol>
-                        <h2>Cody Hoover</h2>
-                    </MDBCol>
-                    <MDBCol>
-                        <MDBRow>
-                            <MDBCol>Portfolio</MDBCol>
-                            <MDBCol>About</MDBCol>
-                            <MDBCol>Contact</MDBCol>
-                        </MDBRow>
+class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false,
+        };
+        this.onClick = this.onClick.bind(this);
+    }
 
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
-       
-    </>
+    onClick() {
+        this.setState({
+            collapse: !this.state.collapse,
+        });
+    }
 
-    )
-};
+    render() {
+
+
+        const container = { height: "1300" }
+        const bgLightBlue = {};
+        const bgBlue = { backgroundColor: '#1565C0' }
+        return (
+            <div>
+                <Router>
+
+                    <MDBNavbar style={bgBlue} dark expand="md" scrolling fixed="top">
+                        <MDBNavbarBrand href="/" className="codyMargin">
+                            <p>CODY HOOVER</p>
+                        </MDBNavbarBrand>
+                        <MDBNavbarToggler onClick={this.onClick} />
+                        <MDBCollapse isOpen={this.state.collapse} navbar>
+                            <MDBNavbarNav right className="linkgroup">
+                                <MDBNavItem>
+                                    <MDBNavLink to="#" className="link">PORTFOLIO</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#" className="link">ABOUT</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#" className="link">CONTACT</MDBNavLink>
+                                </MDBNavItem>
+                            </MDBNavbarNav>
+                        </MDBCollapse>
+                    </MDBNavbar>
+                </Router>
+            
+            </div>
+        );
+    }
+}
 
 export default Nav;
