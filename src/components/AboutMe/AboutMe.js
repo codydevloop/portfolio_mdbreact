8b920eec272690f6dbe4ee661eaaf8ff1fd189a7
CodyHoover <codydevloop@gmail.com>
2020-07-26 06:35:36 -0700

    AboutMe 50% done

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -7,83 +7,107 @@ const AboutMe = () => {
         <>
             <MDBContainer fluid>
                 <MDBRow className=" test mt-5 pt-5">
-                    <MDBCol md="4" className="">
-                        <div className="d-flex flex-row">
-                            <h4 className="popularTeal">class</h4>
-                            <h4 className="popularGreen">CodyHoover</h4>
-                            <h4 className="curlyClass"> {"{"}</h4>
-                        </div>
-                        <div className="d-flex flex-row">
-                            <h4 className="functionBrackets ti">constructor</h4>
-                            <h4 className="purpleBrackets"> ()</h4>
-                            <h4 className="purpleCurly"> {"{"}</h4>
-                        </div>
-                        <div className="d-flex flex-row justify-content-end">
-                            <h4 className="popularTeal">this</h4>
-                            <h4 className="objpropKey mr-4">.bootcampMERN</h4>
-                            <h4 className="equalsWhite"> =</h4>
-                        </div>
-                    </MDBCol>
-                    <MDBCol md="4" className="">
-                        <div className="d-flex flex-row justify-content-start">
-                            <h4 className="objpropVal ">`Completion Date 8/21/20`</h4>
-                            <h4 className="equalsWhite">;</h4>
-                        </div>
-                        <div className="d-flex flex-row">
-                            <h4 className="objpropVal">`Completion Date 8/21/20`</h4>
-                            <h4 className="equalsWhite">;</h4>
-                        </div>
-                        <div className="d-flex flex-row">
-                            <h4 className="objpropVal">`Completion Date 8/21/20`</h4>
-                            <h4 className="equalsWhite">;</h4>
-                        </div>
-                        <div>
-                            <h4></h4>
-                        </div>
-
-
-                        
-                    </MDBCol>
-                        <MDBCol md="4" className="">
-                            <div className="">
-                                <div className="">
-                                    <img src={Cody} className={"img-fluid"} alt="head shot of Cody Hoover" />
+                    <MDBCol md="8" className="">
+                        <MDBRow>
+                            <MDBCol md="12" className="offset-0">
+                                <div className="d-flex flex-row">
+                                    <h4 className="popularTeal mr-2">class</h4>
+                                    <h4 className="popularGreen mr-2">CodyHoover</h4>
+                                    <h4 className="curlyClass"> {"{"}</h4>
                                 </div>
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol md="12" className="offset-1">
+                                <div className="d-flex flex-row">
+                                    <h4 className="functionBrackets mr-2">constructor</h4>
+                                    <h4 className="purpleBrackets mr-2"> ()</h4>
+                                    <h4 className="curlyClass"> {"{"}</h4>
+                                </div>
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol lg="6">
+                                <div className="d-flex flex-row justify-content-end">
+                                    <h4 className="popularTeal">this</h4>
+                                    <h4 className="objpropKey">.bootcampMERN</h4>
+                                    <h4 className="equalsWhite equalsTab">=</h4>
+                                </div>
+                            </MDBCol>
+                            <MDBCol lg="6">
+                                <div className="d-flex flex-row justify-content-start">
+                                    <h4 className="objpropVal">`Completion Date 8/21/20`</h4>
+                                    <h4 className="equalsWhite">;</h4>
+                                </div>
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol lg="6">
+                                <div className="d-flex flex-row justify-content-end">
+                                    <h4 className="popularTeal">this</h4>
+                                    <h4 className="objpropKey">.frontEnd</h4>
+                                    <h4 className="equalsWhite equalsTab">=</h4>
+                                </div>
+                            </MDBCol>
+                            <MDBCol lg="6">
+                                <div className="d-flex flex-row justify-content-start">
+                                    <h4 className="objpropVal">`React, JavaScript, Material`</h4>
+                                    <h4 className="equalsWhite">;</h4>
+                                </div>
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol lg="6">
+                                <div className="d-flex flex-row justify-content-end">
+                                    <h4 className="popularTeal">this</h4>
+                                    <h4 className="objpropKey">.backEnd</h4>
+                                    <h4 className="equalsWhite equalsTab">=</h4>
+                                </div>
+                            </MDBCol>
+                            <MDBCol lg="6">
+                                <div className="d-flex flex-row justify-content-start">
+                                    <h4 className="objpropVal">`Node/Express, MongoDB, SQL`</h4>
+                                    <h4 className="equalsWhite">;</h4>
+                                </div>
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol lg="6">
+                                <div className="d-flex flex-row justify-content-end">
+                                    <h4 className="popularTeal">this</h4>
+                                    <h4 className="objpropKey">.apiDesign</h4>
+                                    <h4 className="equalsWhite equalsTab">=</h4>
+                                </div>
+                            </MDBCol>
+                            <MDBCol lg="6">
+                                <div className="d-flex flex-row justify-content-start">
+                                    <h4 className="objpropVal">`Create, Read, Update, Delete`</h4>
+                                    <h4 className="equalsWhite">;</h4>
+                                </div>
+                            </MDBCol>
+                        </MDBRow>
+
+                        <MDBRow>
+                            <MDBCol></MDBCol>
+                            <MDBCol></MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol></MDBCol>
+                            <MDBCol></MDBCol>
+                        </MDBRow>
+
+
+
+                    </MDBCol>
+                    <MDBCol md="" className="">
+                        <div className="">
+                            <div className="">
+                                <img src={Cody} className={"img-fluid"} alt="head shot of Cody Hoover" />
                             </div>
-                        </MDBCol>
+                        </div>
+                    </MDBCol>
                 </MDBRow>
             </MDBContainer>
-
-
-                {/* <h4>
-                                        <span className="popularTeal">class</span>
-                                        <span className="popularGreen"> CodyHoover</span>
-                                        <span className="curlyClass"> {"{"}</span>
-                                    </h4>
-   
-                                    <h4 className="ti">
-                                        <span className="functionBrackets">constructor</span>
-                                        <span className="purpleBrackets"> ()</span>
-                                        <span className="purpleCurly"> {"{"}</span>
-                                    </h4>
-
-
-                                        <div  className="popularTeal">this</div>
-                                        <div  className="objpropKey">.bootcampMERN</div>
-                                        <div  className="equalsWhite"> =</div>
-  
-                                    <h4>
-                                        <span className="objpropVal">`Completion Date 8/21/20`</span>
-                                        <span className="equalsWhite">;</span>
-                                    </h4>
-                        
-    
-                        <div class="col">
-                            <img src={Cody} className={"img-fluid"} alt="head shot of Cody Hoover" />
-                        </div> */}
-
-
-
         </>
 
     )
