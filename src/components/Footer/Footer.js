67198dd67d5b3f84c3cfe39a5186b0b7e2369eb2
CodyHoover <codydevloop@gmail.com>
2020-07-29 14:25:27 -0700

    updated links to icons in footer, linked updated resume

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { MDBCol, MDBIcon, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import "../Footer/Footer.css";
+import Resume from "../../assets/CH_LHHResPE.pdf";
+
 
 const FooterPage = () => {
     return (
         <div className="backgroundFooter">
             <MDBRow className="">
                 <MDBCol className="centerContent">
-                    <MDBIcon className="icons" fab icon="linkedin-in" />
-                    <MDBIcon className="icons" far icon="file-pdf" />
-                    <MDBIcon className="icons" fab icon="github" />
+                    <a href="https://www.linkedin.com/in/codydevloop" target="_blank"><MDBIcon className="icons" fab icon="linkedin-in"></MDBIcon></a>
+                    <a href={Resume} target="_blank"><MDBIcon far icon="file-pdf" MDBIcon className="icons"/></a>
+                    <a href="https://github.com/codydevloop" target="_blank"><MDBIcon className="icons" fab icon="github" /></a>
                 </MDBCol>
 
             </MDBRow>
